e5d21456a4378b7e5ee79324cf6da5d8077a5d64
kinjal-savitara <kinjal@savitarainfotel.com>
2025-08-12 12:35:07 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,46 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import  { useState, useEffect }  from "react";
 
 export default function Home() {
+ const [formData, setFormData] = useState({ name: "", mobile: "" });
+  const [submitted, setSubmitted] = useState(false);
+
+   useEffect(() => {
+    document.documentElement.style.height = "100%";
+    document.body.style.height = "100%";
+    document.body.style.margin = "0";
+  }, []);
+
+   
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Replace with actual API call or logic
+    console.log("Form submitted:", formData);
+    setSubmitted(true);
+  };
+  
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+     <div style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "100%", zIndex: 9999 }}>
+      <iframe
+        src="https://tours.savitarrealty.in/embed/tour/2c896e83"
+        frameBorder="0"
+        scrolling="no"
+        width="100%"
+        height="100%"
+        allow="vr;xr-spatial-tracking;gyroscope;accelerometer;magnetometer;"
+        title="Sahjanand Marigold"
+        style={{ display: "block", border: "none" }}
+      ></iframe>
     </div>
+
+
+   
   )
 }
